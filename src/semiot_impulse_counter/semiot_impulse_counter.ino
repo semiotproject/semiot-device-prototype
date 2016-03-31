e47cc929529d808cb9ab46295aaf5e5097eef922
Alexey Andreyev <yetanotherandreyev@gmail.com>
2016-03-31 17:23:06 +0300

    working on impulse counter

--- src/semiot_impulse_counter/semiot_impulse_counter.ino
+++ src/semiot_impulse_counter/semiot_impulse_counter.ino
@@ -1,78 +1,84 @@
 // ESP-8266 WiFi-module based
 // Impulse counter
+// That save value to EEPROM
+// And sends current values via UDP
+// to automatically recognized local server
+
+#include "WiFiex.h"
 #include "SemIoTGatewayClient.h"
 
 // Valtec VLF-R-I: "VLFR"
 // Incotex Mercury M201: "M201"
-#define MODEL_WORD "VLFR"
+char _modelWord[] = "VLFR";
+
 #define PULSE_PIN 2 // GPIO2
 
-bool _debug = false;
+bool _debug = true;
 #define SERIAL_BAUDRATE 115200 // debug port
 // debug led to VCC:
-#define DEBUG_LED_PIN 0 // GPIO0
-
-#define MAX_COUNTER_LOW_NUMBER 10
+#define DEBUG_LED_LIGHT LOW
+#define DEBUG_LED_DARK HIGH
+#define DEBUG_LED_PIN 0 // GPIO0; -1 to disable
+HardwareSerial *_debugSerial = &Serial;
 
 WiFiUDP _udp;
 int _udpPort = 33333;
+
+unsigned int _counter = 0;// 4 bytes for esp8266
+bool _counter_changed = false;
+bool _need_to_reconnect = false;
+
 SemIoTGatewayClient *semiotGtwClient;
 
-uint16_t low_counter = 0; // 2 bytes
-unsigned int high_counter = 0; // 4 bytes for esp8266
-bool counter_changed = false;
-bool need_to_reconnect = false;
-
-void blink() {
-    low_counter++;
-    counter_changed = true;
+void _iterate_counter() {
+    _counter++;
+    _counter_changed = true;
 }
 
 void setup() {
-    if (_debug) {
-        Serial.begin(SERIAL_BAUDRATE);
-        semiotGtwClient = new SemIoTGatewayClient(&_udp,_udpPort, &Serial,DEBUG_LED_PIN);
+
+    if (_debugSerial) {
+        _debugSerial->begin(SERIAL_BAUDRATE);
     }
-    else {
-        semiotGtwClient = new SemIoTGatewayClient(&_udp,_udpPort);
-    }
-    // TODO:
-    //EEPROM.begin(16);
-    //readCountersFromEeprom();
+    semiotGtwClient = new SemIoTGatewayClient(&_udp,_udpPort,_debugSerial,DEBUG_LED_PIN);
+    // TODO: EEPROM
     pinMode(PULSE_PIN, INPUT);
     digitalWrite(PULSE_PIN,LOW);
-    attachInterrupt(digitalPinToInterrupt(PULSE_PIN), blink, RISING);
+    attachInterrupt(digitalPinToInterrupt(PULSE_PIN), _iterate_counter, RISING);
+    if (_debugSerial) {
+        Serial.println("Trying to connect to WPS");
+    }
+    while (WiFi.status() != WL_CONNECTED) {
+        connectToWPS(_debugSerial,DEBUG_LED_PIN); // TODO: async via udp SoftAP
+    }
+    if (_debugSerial) {
+        Serial.println("Connected via WPS, searching for local server");
+    }
     semiotGtwClient->connectToSemIoTGateway();
-    if (_debug) {
-        Serial.println("Setup completed, waiting for rising input");
+    if (_debugSerial) {
+        Serial.println("Setup completed");
     }
 }
 
 void loop() {
-    if (need_to_reconnect==true) {
-        need_to_reconnect = false;
+    if (_need_to_reconnect==true) {
+        _need_to_reconnect = false;
         semiotGtwClient->connectToSemIoTGateway();
     }
-    if (counter_changed==true) {
-        counter_changed=false;
-        if (low_counter==MAX_COUNTER_LOW_NUMBER) {
-            low_counter=0;
-            high_counter++;
-        }
-        // writeCountersToEeprom();
-        // send some data
+    if (_counter_changed==true) {
+        _counter_changed=false;
+        // FIXME: check for overflow
+        // TODO: writeCountersToEeprom();
         if (WiFi.status() == WL_CONNECTED) {
             if (!_udp.beginPacket(semiotGtwClient->gatewayIp(), _udpPort)) {
-                need_to_reconnect=true;
+                _need_to_reconnect=true;
             }
-            _udp.write(MODEL_WORD);
-            _udp.write((low_counter >> 8) & 0xFF);
-            _udp.write((low_counter >> 0) & 0xFF);
-
-            _udp.write((high_counter >> 24) & 0xFF);
-            _udp.write((high_counter >> 16) & 0xFF);
-            _udp.write((high_counter >> 8) & 0xFF);
-            _udp.write((high_counter >> 0) & 0xFF);
+            _udp.write(_modelWord);
+            // TODO: separate to lib:
+            _udp.write((_counter >> 24) & 0xFF);
+            _udp.write((_counter >> 16) & 0xFF);
+            _udp.write((_counter >> 8) & 0xFF);
+            _udp.write((_counter >> 0) & 0xFF);
             byte *mac = semiotGtwClient->mac();
             // NOTE: danger access?
             _udp.write(mac[0]);
@@ -81,18 +87,16 @@ void loop() {
             _udp.write(mac[3]);
             _udp.write(mac[4]);
             _udp.write(mac[5]);
-            if (_udp.endPacket()) {
-                need_to_reconnect=true;
+            if (!_udp.endPacket()) {
+                _need_to_reconnect=true;
             }
-            if (_debug) {
-                Serial.print("counter = ");
-                Serial.println(low_counter,DEC);
-                Serial.print("high counter = ");
-                Serial.println(high_counter,DEC);
+            if (_debugSerial) {
+                _debugSerial->print("counter = ");
+                _debugSerial->println(_counter,DEC);
             }
         }
         else {
-            need_to_reconnect=true;
+            _need_to_reconnect=true;
         }
     }
 }
